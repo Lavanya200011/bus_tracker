10d0ece91e7cd1d252d84de9be43cadf8ac14bb2
lavanya <lavanyathawkar@gmail.com>
2026-03-26 23:40:42 +0530

    no update

--- client/src/utils/routes.js
+++ client/src/utils/routes.js
@@ -1,6 +1,6 @@
 export const BUS_ROUTES = [
-  { id: '402-B', name: 'Sakoli ↔ Bhandara', color: 'bg-blue-600' },
-  { id: '101-A', name: 'bhsndara ↔ Sakoli', color: 'bg-green-600' },
-  { id: 'CAMPUS-S', name: 'Bhandara ↔ Ambhora', color: 'bg-purple-600' },
-  { id: 'CAMPUS-S', name: 'Ambhora ↔ Bhandara', color: 'bg-purple-600' },
+  { id: 'Sakoli -> Bhandara', name: 'Sakoli -> Bhandara', color: 'bg-blue-600' },
+  { id: 'bhandara -> Sakoli', name: 'bhandara -> Sakoli', color: 'bg-green-600' },
+  { id: 'Bhandara -> Ambhora', name: 'Bhandara -> Ambhora', color: 'bg-purple-600' },
+  { id: 'Ambhora -> Bhandara', name: 'Ambhora -> Bhandara', color: 'bg-purple-600' },
 ];
